fcded05b14e591d07ad5a35d5cc5fc1820fbba07
Oussama Oulkaid <oussama.oulkaid@gmail.com>
2021-11-20 11:56:02 +0100

    changed script & commit analysis

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -1,12 +1,24 @@
 
 OUTPUT_DIRECTORY=data/`hostname`_`date +%F`
 mkdir -p $OUTPUT_DIRECTORY
-OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
+OUTPUT_TXT=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
+OUTPUT_CSV=$OUTPUT_DIRECTORY/measurements_`date +%R`.csv
+c100=100
 
-touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000; do
+START_SIZE=0
+MAX_SIZE=15000000
+STEP=1000000
+
+touch $OUTPUT_TXT
+for i in $(eval echo {$START_SIZE..$MAX_SIZE..$STEP} ); do
     for rep in `seq 1 5`; do
-        echo "Size: $i" >> $OUTPUT_FILE;
-        ./src/parallelQuicksort $i >> $OUTPUT_FILE;
+        echo "Size: $i" >> $OUTPUT_TXT;
+        ./src/parallelQuicksort $i >> $OUTPUT_TXT;
     done ;
+    echo -ne "Progress: $((i * c100 / MAX_SIZE))%\r"
 done
+echo -ne "Progress: 100% --> generating csv file\n"
+
+perl scripts/csv_quicksort_extractor2.pl < $OUTPUT_TXT > $OUTPUT_CSV
+echo "Output: $OUTPUT_CSV"
+rm $OUTPUT_TXT
